b14bee3c8372e378a2158218263ef464a64b0902
Anushree <anushreepm13@gmail.com>
2020-09-07 12:14:58 +0530

    added header kannada in index.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
